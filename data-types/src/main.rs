6e88e4bc23687c8adea5a005bb2e7e89eccecc42
Thierry Santos <thierrysantoos123@gmail.com>
2021-08-01 10:33:11 -0300

    compound data types

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -17,8 +17,28 @@ fn main() {
     let t = true;
     let f: bool = false; // explicit type annotation
 
-
     // char types
     let c = 'z';
     let cat_emoji = '😻';
+
+    // compound types
+    // we have two primitive types: tuples and arrays
+    // tuples can have more than one type
+    // tuples cannot grow or shrink in size
+    let tuple: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (_, k, _) = tuple;
+
+    println!("The value of k is: {}", k);
+    println!("The value of tuple.1 is: {}", tuple.1);
+
+    // array can have only one type
+    // different from other languages, array have fixed size
+    // stored on the stack
+    let arr = [1,2,3,4,5];
+    // typing and array and specifying the size
+    let arr_typed: [i32; 5] = [1,2,3,4,5];
+    // array initialized
+    // all values equal to 3 and length 5
+    let a_initialized = [3; 5];
 }
