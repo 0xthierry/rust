e2f737e0c7214d00a0250c15d5a59e14db211ed5
Thierry Santos <thierrysantoos123@gmail.com>
2021-08-17 19:39:32 -0300

    conditions

---
+++ conditions/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 5;
+
+    if x < 10 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let y = if x > 10 { true } else {false};
+}
